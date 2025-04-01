017047274fbfed59b988ff89944faf3ec31e5d24
Drtikolnik <drtluk00@gasos-ro.cz>
2025-04-01 07:59:12 +0200

    finally?

--- src/Main2.java
+++ src/Main2.java
@@ -8,7 +8,10 @@ public class Main2 {
 
         ArrayList<Kniha> knihy = new ArrayList<>();
 
+        //zadání
         System.out.println("ZADÁVEJ OBLÍBENÉ KNIHY. PRO UKONČENÍ ZADEJ MÍSTO NÁZVU KNIHY stop");
+        System.out.println(" ");
+
         for(boolean i = false; !i ;) {
             System.out.println("Zadej název knihy:");
             String nazev = sc.nextLine();
@@ -27,23 +30,53 @@ public class Main2 {
 
         }
 
+        System.out.println(" ");
         System.out.println("Oblíbené knihy: ");
         for (Kniha kniha : knihy) {
             System.out.println("Kniha: " +kniha.getNazev()+ " autor: " +kniha.getAutor()+ " rok vydání: " +kniha.getRokVydani());
         }
 
-        //odebírání
-        System.out.println("Jaký název knihy by jsi chtěl Změnit? - číslo 1 pro 1. knihu atd.");
-        int zmena = sc.nextInt() -1;
-        sc.nextLine();
-        System.out.println("Napiš nové jméno");
-        String novyNazev = sc.nextLine();
-        knihy.get(zmena).setNazev(novyNazev);
+        //změnění
+        System.out.println(" ");
+        System.out.println("CHCEŠ ZMĚNIT NĚKTERÝ NÁZEV KNIHY? - ano / ne");
+        String zmenaJmena = sc.nextLine();
 
-        System.out.println("Oblíbení knihy po změně: ");
-        for (Kniha kniha : knihy) {
-            System.out.println("Kniha: " +kniha.getNazev()+ " autor: " +kniha.getAutor()+ " rok vydání: " +kniha.getRokVydani());
+        switch (zmenaJmena) {//.ů
+            case "ano":
+                System.out.println("Jaký název knihy by jsi chtěl Změnit? - číslo 1 pro 1. knihu atd.");
+                int zmena = sc.nextInt() -1;
+                sc.nextLine();
+                System.out.println("Napiš nové jméno");
+                String novyNazev = sc.nextLine();
+                knihy.get(zmena).setNazev(novyNazev);
+
+                System.out.println("Oblíbení knihy po změně: ");
+                for (Kniha kniha : knihy) {
+                    System.out.println("Kniha: " +kniha.getNazev()+ " | autor: " +kniha.getAutor()+ " | rok vydání: " +kniha.getRokVydani());
+                }
+                break;
         }
+
+
+        //podle autora
+        System.out.println(" ");
+        System.out.println("CHCEŠ ZOBRAZIT KNIHY PODLE AUTORA? - ano / ne");
+        String zobrazitPodleAutora = sc.nextLine();
+
+        switch (zobrazitPodleAutora) {
+            case "ano":
+                System.out.println("Od jakého autora chceš zobrazit knihy? - zadej jméno autora");
+                String jmenoAutora = sc.nextLine();
+
+                System.out.println("Výpis knih od autora " +jmenoAutora);
+                for (Kniha kniha : knihy) {
+                    if(jmenoAutora.equals(kniha.getAutor())){
+                        System.out.println("Kniha: " +kniha.getNazev()+ " | autor: " +kniha.getAutor()+ " | rok vydání: " +kniha.getRokVydani());
+                    }
+                }
+                break;
+        }
+
 
 
 
@@ -100,13 +133,6 @@ public class Main2 {
 
 
     }
-
-
-
-
-
-
-
 
 
 }
